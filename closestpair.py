578e4699e33e977e407e0b31cec108a834fe7d49
Guofeng Chen <chenguofeng08qh@gmail.com>
2013-10-04 17:28:49 +0800

    add a method to find closest pair; code from the Internet

--- closestpair.py
+++ closestpair.py
@@ -1,19 +1,25 @@
 #coding: utf-8
 import Tkinter
+from closest import closestpair
 class Tkwin:
     def __init__(self, root):
         self.root = root
         self.points = []
+        self.line = None
         self.root.bind('<Button-1>', self.action)
     def action(self, event):
         s = "Last point clicked at x=%s  y=%s" % (event.x, event.y)
         self.root.title(s)
+        w.create_rectangle(event.x, event.y, event.x, event.y)
         self.points.append([event.x, event.y])
         if len(self.points) >=2 :
             self.closest_pair()
     def closest_pair(self):
-        w.create_line(self.points[len(self.points)-1][0], self.points[len(self.points)-1][1], self.points[len(self.points)-2][0],self.points[len(self.points)-2][1])
-
+        if self.line is not None:
+            w.delete(self.line)
+        ans = closestpair(self.points)
+        self.line = w.create_line(ans[0][0], ans[0][1], ans[1][0], ans[1][1])
+        
 
 root = Tkinter.Tk()
 
